618cdb952dc2e2b2af7005232dc7cae70397a400
trentonropp <trentonropp@modem-6.local>
2017-01-23 13:56:15 -0500

    update
    updated some last minute things before the final judging round

--- slideAnimation.js
+++ slideAnimation.js
@@ -3,8 +3,24 @@
 $( document ).ready(function() {
     $( "#submit" ).click(function() {
         window.location.href='loading.html';
-        
     });
+    $( "#button1" ).click(function() {
+        window.location.href='http://i.imgur.com/A5pmUn4.png';
+    });
+    $( "#button2" ).click(function() {
+        window.location.href='http://www.huffingtonpost.com/entry/obama-pledge-of-allegiance_us_57c9bc7ae4b0e60d31defd6d';
+    });
+    $( "#button3" ).click(function() {
+        window.location.href='http://abcnews.com.co/obama-executive-order-bans-pledge-of-allegiance-in-schools/';
+    });
+    $( "#button4" ).click(function() {
+        window.location.href='http://www.factcheck.org/2016/09/obama-did-not-ban-the-pledge/';
+    });
+    $( "#button5" ).click(function() {
+        window.location.href='http://www.cnn.com/2013/09/04/us/pledge-of-allegiance-fast-facts/';
+    });
+    
+    
     
 
 
@@ -21,6 +37,8 @@ $( document ).ready(function() {
     $('#button3').append(data.feed.entry[0]['gsx$slider3']['$t']);
     $('#button4').append(data.feed.entry[0]['gsx$slider4']['$t']);
     $('#button5').append(data.feed.entry[0]['gsx$slider5']['$t']);
+    
+    
   //console.log(data);
  //loop
     
